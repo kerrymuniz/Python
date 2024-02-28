e59ee850de569fe57f14e868b7e55e4499d244ba
kerrymuniz <132500154+kerrymuniz@users.noreply.github.com>
2024-02-28 16:57:58 -0300

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,16 +1,17 @@
 #IMPORTANDO A BIBLIOTECA RANDOM + FUNÇÃO RANDINT()
 import random
 random_choice = random.randint(0,2)
-print("O computador escolheu o nº " + str(random_choice))
+""" print("O computador escolheu o nº " + str(random_choice)) """
 
 #BOOLEANO
-gols_jogador1 = 5
+""" gols_jogador1 = 5
 gols_jogador2 = 3
 booleano = gols_jogador1 > gols_jogador2
-print(booleano)
+print(booleano) """
 
 #if else
 #EX1:
+"""
 number_of_scoops = random.randint(0,3)
 print(number_of_scoops)
 if(number_of_scoops == 0):
@@ -24,7 +25,9 @@ elif(number_of_scoops >= 3):
     print("Wow, that's a lot of scoops!")
 else:
     print("I'm sorry I can't give you negative scoops.")
+"""
 #EX2: ROCK, PAPER, SCISSORS
+winner = ''
 if(random_choice == 0):
     computer_choice = 'rock'
 elif(random_choice == 1):
@@ -32,6 +35,18 @@ elif(random_choice == 1):
 else:
     computer_choice = 'scissors'
 
-#print("O computador escolheu", computer_choice)
+print("O computador escolheu", computer_choice,".")
 user_choice = input("rock, paper or scissors? ")
-print("Você escolheu", user_choice, "e o computador escolheu", computer_choice)
+#print("Você escolheu", user_choice, "e o computador escolheu", computer_choice)
+if(computer_choice == user_choice):
+    winner = 'Tie'
+elif(computer_choice == 'paper' and user_choice == 'rock'):
+    winner = 'Computer'
+elif(computer_choice == 'rock' and user_choice == 'scissors'):
+    winner = 'Computer'
+elif(computer_choice == 'scissors' and user_choice == 'paper'):
+    winner = 'Computer'
+else:
+    winner = 'User'
+
+print(winner)
